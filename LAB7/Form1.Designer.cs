35d6b868f509bc94e18e7bfbb4c8768a574509bd
stu-pksp120 <stu-pksp120@HQVLA3227W15>
2023-02-09 11:01:09 +0300

    Добавьте файлы проекта.

---
+++ LAB7/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace LAB7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TextTB = new System.Windows.Forms.TextBox();
+            this.NValueBox = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CipherButton = new System.Windows.Forms.Button();
+            this.DecipherButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.NValueBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Слово";
+            // 
+            // TextTB
+            // 
+            this.TextTB.Location = new System.Drawing.Point(84, 10);
+            this.TextTB.Name = "TextTB";
+            this.TextTB.Size = new System.Drawing.Size(219, 20);
+            this.TextTB.TabIndex = 1;
+            // 
+            // NValueBox
+            // 
+            this.NValueBox.Location = new System.Drawing.Point(84, 37);
+            this.NValueBox.Name = "NValueBox";
+            this.NValueBox.Size = new System.Drawing.Size(219, 20);
+            this.NValueBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Смещение";
+            // 
+            // CipherButton
+            // 
+            this.CipherButton.Location = new System.Drawing.Point(16, 63);
+            this.CipherButton.Name = "CipherButton";
+            this.CipherButton.Size = new System.Drawing.Size(140, 23);
+            this.CipherButton.TabIndex = 4;
+            this.CipherButton.Text = "Зашифровать";
+            this.CipherButton.UseVisualStyleBackColor = true;
+            this.CipherButton.Click += new System.EventHandler(this.CipherButton_Click);
+            // 
+            // DecipherButton
+            // 
+            this.DecipherButton.Location = new System.Drawing.Point(162, 63);
+            this.DecipherButton.Name = "DecipherButton";
+            this.DecipherButton.Size = new System.Drawing.Size(141, 23);
+            this.DecipherButton.TabIndex = 5;
+            this.DecipherButton.Text = "Расшифровать";
+            this.DecipherButton.UseVisualStyleBackColor = true;
+            this.DecipherButton.Click += new System.EventHandler(this.DecipherButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 97);
+            this.Controls.Add(this.DecipherButton);
+            this.Controls.Add(this.CipherButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.NValueBox);
+            this.Controls.Add(this.TextTB);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.NValueBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TextTB;
+        private System.Windows.Forms.NumericUpDown NValueBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button CipherButton;
+        private System.Windows.Forms.Button DecipherButton;
+    }
+}
+
---
+++ LAB7/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB7
+{
+    public partial class Form1 : Form
+    {
+        char[] alphabet = { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я' };
+        char[] Cipher(char[] text, int n)
+        {
+            int nom; // Номер в алфавите
+            int j; // Переменная для циклов
+            int smesch = n;
+
+            // Перебираем каждый символ сообщения
+            for (int i = 0; i < text.Length; i++)
+            {
+                
+                // Ищем индекс буквы
+                for (j = 0; j < alphabet.Length; j++)
+                {
+                    if (text[i] == alphabet[j])
+                    {
+                        break;
+                    }
+                }
+
+                if (j != 33) // Если j равно 33, значит символ не из алфавита
+                {
+                    nom = j; // Индекс буквы
+                    smesch = nom + n; // Делаем смещение
+
+                    // Проверяем, чтобы не вышли за пределы алфавита
+                    if (smesch > 32)
+                    {
+                        smesch = smesch - 33;
+                    }
+
+                    text[i] = alphabet[smesch]; // Меняем букву
+                }
+            }
+            return text;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CipherButton_Click(object sender, EventArgs e)
+        {
+            char[] text = TextTB.Text.ToLower().ToCharArray();
+            int n = Convert.ToInt32(NValueBox.Value);
+            
+            MessageBox.Show(new string(Cipher(text, n)));
+        }
+
+        private void DecipherButton_Click(object sender, EventArgs e)
+        {
+            char[] text = TextTB.Text.ToLower().ToCharArray();
+            int n = 33 - Convert.ToInt32(NValueBox.Value);
+            MessageBox.Show(new string(Cipher(text, n)));
+        }
+    }
+}
